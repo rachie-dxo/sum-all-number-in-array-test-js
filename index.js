9e9f4c901c5ee961e3901b215df4ccbda1f5e6d7
Rachel Dallaire <rachel-dallaire@outlook.com>
2018-02-14 23:50:56 +0000

    Sums all numbers in arrays, even if nested

--- index.js
+++ index.js
@@ -1,5 +1,15 @@
 function sumItems(array) {
   // Sum all the numbers in the array
+  let total = 0;
+  array.forEach((item) => {
+    if (Array.isArray(item)) {
+      // Print out all its items individually
+       total += sumItems(item);
+    } else {
+      total += item;
+    }
+  });
+  return total;
 }
 
 module.exports = sumItems;
